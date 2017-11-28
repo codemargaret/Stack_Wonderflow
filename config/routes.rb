593996219dfbb1b826ace399a2691df3811ec70e
Michael Brooks and Margaret Berry <margaret@>
2017-11-28 14:11:00 -0800

    add bootstrap without sprockets or sass, working index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  root :to => 'questions#index'
+
+  resources :questions do
+    resources :answers
+  end
+
+  resources :questions do
+    resources :comments
+  end
+
+  resources :answers do
+    resources :answer_comments
+  end
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
