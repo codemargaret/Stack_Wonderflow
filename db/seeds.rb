a13047b1baf971dba63f182b6692fac6b56b984f
Michael Brooks and Margaret Berry <michael@>
2017-11-28 16:19:29 -0800

    add seed file components and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,71 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+created_users = 0
+while created_users < 5
+  user = User.new(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    )
+    created_users += 1 if user.save
+end
+p "You now have #{User.count} users in your db"
+
+
+Question.destroy_all
+
+10.times do
+  random_user = User.all.sample
+  random_user.questions.create(
+    title: Faker::FamilyGuy.quote,
+    text: Faker::Hipster.sentences(2),
+    rating: Faker::Number.between(1, 100)
+    )
+  end
+
+p "You have created #{Question.count} questions in your db"
+
+
+
+
+
+Answer.destroy_all
+
+10.times do
+  random_user = User.all.sample
+  random_question = Question.all.sample
+  random_question.answers.create(
+    text: Faker::ChuckNorris.fact,
+    rating: Faker::Number.between(1, 100),
+    user_id: random_user.id
+    )
+end
+
+p "You now have #{Answer.count} answers in your db"
+
+
+Comment.destroy_all
+
+25.times do
+  random_user = User.all.sample
+  random_question = Question.all.sample
+  random_question.comments.create(
+    text: Faker::Hipster.sentences(1),
+    user_id: random_user.id
+    )
+end
+
+p "You now have #{Comment.count} comments in your db"
+
+
+AnswerComment.destroy_all
+
+25.times do
+  random_user = User.all.sample
+  random_answer = Answer.all.sample
+  random_answer.answer_comments.create(
+    text: Faker::Hipster.sentences(1),
+    user_id: random_user.id
+    )
+end
+
+p "You now have #{AnswerComment.count} answer_comments in your db"
